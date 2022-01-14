9fe4f93a49d8026b1444235c94710ef1f97445b7
ZQR0 <yarikrech@gmail.com>
2022-01-14 23:59:16 +0300

    realease 1.0.0

---
+++ NetSaver/NetSaver.py
@@ -0,0 +1,36 @@
+import requests
+
+class Net_Saver:
+
+    """URL parameter is ur file url"""
+    def __init__(self, url: str):
+        self.response = requests.get(url=url)
+
+    #To save image
+    def image_safe(self, file_name):
+        with open(file=f'{file_name}.jpeg', mode='wb') as file:
+            file.write(self.response.content)
+            file.close()
+            
+            return "Succesfully saved!!!"
+
+    #To save video
+    def video_save(self, file_name):
+        with open(file=f'{file_name}.mp4', mode='wb') as file:
+            file.write(self.response.content)
+            file.close()
+
+            return "Succesfully saved!!!"
+
+    #To save gif
+    def gif_save(self, file_name):
+        with open(file=f'{file_name}.gif', mode='wb') as file:
+            file.write(self.response.content)
+            file.close()
+
+            return "Succesully saved"
+
+
+#Choose the function and use it with print
+if __name__ == '__main__':
+    pass
